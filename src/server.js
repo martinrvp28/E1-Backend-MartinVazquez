2d51d32c1e11c106f1ead1b61857e542875d9725
martin vazquez <martinrvp28@gmail.com>
2023-07-04 17:06:59 -0300

    adding plantillas

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import morgan from 'morgan';
 import { errorHandler } from './middlewares/errorHandler.js';
 import productsRouter from './routes/productsRouter.js';
 import cartRouter from './routes/cartRouter.js';
+import handlebars from 'express-handlebars';
+import viewsRouter from './routes/views.router.js';
+import { __dirname } from "../utils.js";
 
 const app = express();
 
@@ -11,8 +14,14 @@ app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 app.use(errorHandler);
 
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/src/views');
+app.set('view engine', 'handlebars');
+
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartRouter);
+
+app.use('/', viewsRouter);
 
 const PORT = 8080;
 
